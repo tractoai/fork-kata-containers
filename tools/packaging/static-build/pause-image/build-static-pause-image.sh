1a4142509527c4ff714fb4adb1a8d3a4ca8aa968
Konstantin Khlebnikov <koct9i@gmail.com>
2024-07-03 20:05:45 +0200

    CI: Fix rebuilding pause-image-tarball
    Cleanup previous build at "make pause-image-tarball".
    
    Signed-off-by: Konstantin Khlebnikov <koct9i@gmail.com>

--- tools/packaging/static-build/pause-image/build-static-pause-image.sh
+++ tools/packaging/static-build/pause-image/build-static-pause-image.sh
@@ -18,6 +18,7 @@ source "${script_dir}/../../scripts/lib.sh"
 pull_pause_image_from_remote() {
 	echo "pull pause image from remote"
 
+	rm -fr "${DESTDIR}/pause_bundle"
 	skopeo copy "${pause_image_repo}":"${pause_image_version}" oci:pause:"${pause_image_version}"
 	umoci unpack --rootless --image pause:"${pause_image_version}"  "${DESTDIR}/pause_bundle"
 	rm "${DESTDIR}/pause_bundle/umoci.json"
